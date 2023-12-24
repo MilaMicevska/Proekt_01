a249782f58f15e70debdf316813af814f85f6214
MilaMicevska <micevska.mila@gmail.com>
2023-12-24 10:05:20 +0100

    First commit

---
+++ src/Smetka.java
@@ -0,0 +1,15 @@
+
+public class Smetka {
+
+	public static void main(String[] args) {
+		int jabolki=32;
+		int kompiri=20;
+		double praski=50;
+		double rezultati=(jabolki*2)+(kompiri*5)+(praski*2.5);
+		System.out.println("smetka");
+		System.out.println(rezultati);
+
+	}
+
+}
+
